20f4a02019f1fdf2138e4e2a8996103e33dd8be6
yogo <47849728+johnnyvaca@users.noreply.github.com>
2022-02-10 16:55:24 +0100

    wip

--- App.js
+++ App.js
@@ -8,66 +8,64 @@
 
 import React, {Component} from 'react';
 import {View, StyleSheet, TouchableOpacity, Text} from 'react-native';
+import CustomButton from './components/button';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0,
-    };
-    this.handleUp = this.handleUp.bind(this);
-    this.handleDown = this.handleDown.bind(this);
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0,
+        };
+        this.handleUp = this.handleUp.bind(this);
+        this.handleDown = this.handleDown.bind(this);
+    }
 
-  handleUp() {
-    const {counter: ct} = this.state;
-    this.setState({counter: ct + 1});
-  }
+    handleUp() {
+        const {counter: ct} = this.state;
+        this.setState({counter: ct + 1});
+    }
 
-  handleDown() {
-    const {counter: ct} = this.state;
-    this.setState({counter: ct - 1});
-  }
+    handleDown() {
+        const {counter: ct} = this.state;
+        this.setState({counter: ct - 1});
+    }
 
-  render() {
-    const {counter} = this.state;
-    return (
-      <View style={desStyles.container}>
-        <View style={desStyles.styleContactor}>
-          <TouchableOpacity style={desStyles.stBtn} onPress={this.handleDown}>
-            <Text style={desStyles.stBtnTxt}>-</Text>
-          </TouchableOpacity>
-          <View style={desStyles.txtCounter}>
-            <Text style={desStyles.txtC}>{counter}</Text>
-          </View>
-          <TouchableOpacity style={desStyles.stBtn}>
-            <Text style={desStyles.stBtnTxt} onPress={this.handleUp}>
-              +
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-    );
-  }
+    render() {
+        const {counter} = this.state;
+        return (
+            <View style={desStyles.container}>
+                <View style={desStyles.styleContactor}>
+
+                    <CustomButton label="-" action={this.handleDown}/>
+                    <View style={desStyles.txtCounter}>
+                        <Text style={desStyles.txtC}>{counter}</Text>
+                    </View>
+                    <CustomButton label="+" action={this.handleUp}/>
+                </TouchableOpacity>
+            </View>
+    </View>
+    )
+        ;
+    }
 }
 
 const desStyles = StyleSheet.create({
-  container: {flex: 1, backgroundColor: '#2c3e50', justifyContent: 'center'},
-  styleContactor: {
-    height: 50,
-    width: '100%',
-    paddingHorizontal: 10,
-    flexDirection: 'row',
-  },
-  stBtn: {
-    width: 50,
-    height: '100%',
-    backgroundColor: '#ecf0f1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  stBtnTxt: {fontSize: 18, color: '#2c3e50', fontWeight: 'bold', opacity: 2},
-  txtCounter: {flex: 1, justifyContent: 'center', alignItems: 'center'},
-  txtC: {fontSize: 40, color: 'white', fontWeight: 'bold'},
+    container: {flex: 1, backgroundColor: '#2c3e50', justifyContent: 'center'},
+    styleContactor: {
+        height: 50,
+        width: '100%',
+        paddingHorizontal: 10,
+        flexDirection: 'row',
+    },
+    stBtn: {
+        width: 50,
+        height: '100%',
+        backgroundColor: '#ecf0f1',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    stBtnTxt: {fontSize: 18, color: '#2c3e50', fontWeight: 'bold', opacity: 2},
+    txtCounter: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+    txtC: {fontSize: 40, color: 'white', fontWeight: 'bold'},
 });
 export default App;
